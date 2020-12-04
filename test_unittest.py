e367095c6db9f9259e3914ca279e22e8fd7d1bdd
Nursultan Karybekov <n.karybekov22@gmail.com>
2020-12-04 18:55:18 +0600

    first commit

---
+++ test_unittest.py
@@ -0,0 +1,29 @@
+import unittest
+import astar
+
+
+def test_h(p1, p2):
+    self.assertEquals(h(10, 10), )
+
+
+
+# class Test_Spot(unittest.TestCase):
+#     def test_is_closed(self):
+#         s = astar.Spot(self, 10, 10, 10, 10)
+#         self.assertEquals(s.is_closed(), True)
+
+#     def test_is_open(self):
+#         s = astar.Spot(10, 10, 10, 10)
+#         self.assertEquals(s.is_open(self), True)
+
+#     def test_is_barrier(self):
+#         s = astar.Spot(10, 10, 10, 10)
+#         self.assertEquals(s.is_barrier(self), True)
+
+#     def test_is_start(self):
+#         s = astar.Spot(10, 10, 10, 10)
+#         self.assertEquals(s.is_start(self), True)
+
+#     def test_is_end(self):
+#         s = astar.Spot(10, 10, 10, 10)
+#         self.assertEquals(s.is_end(self), True)
